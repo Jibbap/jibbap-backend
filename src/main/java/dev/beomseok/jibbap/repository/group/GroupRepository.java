6ebddc0de72f5240e066dd9e4895baa50efe54ed
cotton <bmtosss@gmail.com>
2024-06-27 10:12:45 +0900

    [chore] 주석 제거

--- src/main/java/dev/beomseok/jibbap/repository/group/GroupRepository.java
+++ src/main/java/dev/beomseok/jibbap/repository/group/GroupRepository.java
@@ -6,13 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface GroupRepository extends JpaRepository<GroupEntity,Long>,GroupCustomRepository {
 
     GroupEntity findByUuid(String uuid);
-
-//    @Query(value = "select g.group_name, g.uuid, r.username_in_group, g.id " +
-//            "from jibbap_group g " +
-//            "join relationship r " +
-//            "on r.group_id = g.id " +
-//            "join jibbap_user u " +
-//            "on r.user_id = u.id " +
-//            "where u.kakao_id=:kakaoId",nativeQuery = true)
-//    List<GroupDetail> findGroupDetailByKakaoId(@Param("kakaoId") String kakaoId);
 }
